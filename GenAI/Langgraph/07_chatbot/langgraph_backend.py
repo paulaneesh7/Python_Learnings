f2aa8fb46b6e4ce8a6e624801f2c74e4da37baae
Aneesh Paul <aneesh16117@gmail.com>
2026-07-09 21:15:32 +0530

    Langgraph : streaming-threading

--- GenAI/Langgraph/07_chatbot/langgraph_backend.py
+++ GenAI/Langgraph/07_chatbot/langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -29,4 +29,11 @@ graph.add_edge(START, "chat_node")
 
 graph.add_edge("chat_node", END)
 
+
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
+
+
+
+
